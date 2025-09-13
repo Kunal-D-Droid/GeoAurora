03844bc00234dc776252806c50dda569a3aec52d
Kunal-D-Droid <daskunal536@gmail.com>
2025-09-13 23:54:37 +0530

    map marker fix

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+
+// Fix for default markers in production
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
 
 export default function MapView({ events, heightClass = 'h-96', zoom = 2, className = '' }) {
   // Center on first event if available
